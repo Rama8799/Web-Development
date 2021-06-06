e9b4ba0ae10ff1e9e0c065f4ae36156fb6e28d7b
Rama Subramanian <50733612+Rama8799@users.noreply.github.com>
2021-06-06 21:54:25 +0530

    Delete hq-admin-master directory

--- hq-admin-master/src/js/custom.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    "use strict";
-
-    $('[data-toggle="sidebar"]').click(function(event){
-        event.preventDefault();
-        $('.app').toggleClass('sidebar-toggled');
-        
-    });
-})();

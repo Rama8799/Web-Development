4be61786ec94c82e7e4eb1724b9420917c4dbf81
Rama Subramanian <rama.mit2016@gmail.com>
2021-06-12 22:32:57 +0530

    Final one

--- Covid-tracker/script.js
+++ Covid-tracker/script.js
@@ -12,9 +12,9 @@ country.addEventListener('change', () => {
         .then(res => res.json())
         .then(data => {
             header.innerHTML=`COVID TRACKER <span>${country.value}<span>`;
-            cases.innerHTML = `Cases : ${data.cases} <i class="fas fa-arrow-up fa-sm red"></i> \u00A0 <span class="today">${data.todayCases}</span>`;
-            deaths.innerHTML = `Deaths : ${data.deaths} <i class="fas fa-arrow-up fa-sm red"></i> \u00A0 <span class="today">${data.todayDeaths}</span>`;
-            recovered.innerHTML = `Recovered : ${data.recovered} <i class="fas fa-arrow-up fa-sm green"></i> \u00A0 <span class="today">${data.todayRecovered}</span>`;
+            cases.innerHTML = `Cases : ${data.cases} <i class="fas fa-arrow-up fa-sm red"></i> <span class="today">${data.todayCases}</span>`;
+            deaths.innerHTML = `Deaths : ${data.deaths} <i class="fas fa-arrow-up fa-sm red"></i> <span class="today">${data.todayDeaths}</span>`;
+            recovered.innerHTML = `Recovered : ${data.recovered} <i class="fas fa-arrow-up fa-sm green"></i> <span class="today">${data.todayRecovered}</span>`;
             active_cases.innerHTML = `Active Cases : ${data.active}`;
             critical_cases.innerHTML = `Critical Cases : ${data.critical}`;
         });

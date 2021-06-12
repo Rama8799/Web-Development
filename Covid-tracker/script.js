0c8ebd5fbc40a99d557d19481182b5670076b8d5
Rama Subramanian <rama.mit2016@gmail.com>
2021-06-12 19:59:16 +0530

    Better design

--- Covid-tracker/script.js
+++ Covid-tracker/script.js
@@ -1,9 +1,6 @@
-total_cases = document.getElementById("total_cases");
-today_cases = document.getElementById("today_cases");
-today_deaths = document.getElementById("today_deaths");
-today_deaths = document.getElementById("today_deaths");
-total_recovered = document.getElementById("total_recovered");
-today_recovered = document.getElementById("today_recovered");
+cases = document.getElementById("cases");
+deaths = document.getElementById("deaths");
+recovered = document.getElementById("recovered");
 active_cases = document.getElementById("active_cases");
 critical_cases = document.getElementById("critical_cases");
 country = document.getElementById("country");
@@ -14,13 +11,10 @@ country.addEventListener('change', () => {
     fetch(`https://corona.lmao.ninja/v2/countries/${country.value}?yesterday&strict&query%20`)
         .then(res => res.json())
         .then(data => {
-            header.innerText=`COVID TRACKER - ${country.value}`
-            total_cases.innerHTML = `Total Cases : ${data.cases}`;
-            today_cases.innerHTML = `New Cases : ${data.todayCases}`;
-            total_deaths.innerHTML = `Total Deaths : ${data.deaths}`;
-            today_deaths.innerHTML = `New Deaths : ${data.todayDeaths}`;
-            total_recovered.innerHTML = `Total Recovered : ${data.recovered}`;
-            today_recovered.innerHTML = `Newly Recovered : ${data.todayRecovered}`;
+            header.innerHTML=`COVID TRACKER <span>${country.value}<span>`;
+            cases.innerHTML = `Cases : ${data.cases} <i class="fas fa-arrow-up fa-sm red"></i> \u00A0 <span class="today">${data.todayCases}</span>`;
+            deaths.innerHTML = `Deaths : ${data.deaths} <i class="fas fa-arrow-up fa-sm red"></i> \u00A0 <span class="today">${data.todayDeaths}</span>`;
+            recovered.innerHTML = `Recovered : ${data.recovered} <i class="fas fa-arrow-up fa-sm green"></i> \u00A0 <span class="today">${data.todayRecovered}</span>`;
             active_cases.innerHTML = `Active Cases : ${data.active}`;
             critical_cases.innerHTML = `Critical Cases : ${data.critical}`;
         });
